31d451bd5bd700e6162162ea88579e76055b5192
Samuel Hereira <samuelhereira@gmail.com>
2020-05-15 23:17:10 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ) {
    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Saren:z4YkXWn36hY9ZT3@cluster0-slksd.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
